4aeb2dd6e7ffd939f3d52d36f666c94264c61cdb
pavan kumar <pavan kumar@LAPTOP-QVKC38NK>
2019-08-13 18:49:30 +0530

    commit 1

---
+++ src/com/primitivetypecasting/P1.java
@@ -0,0 +1,31 @@
+package com.primitivetypecasting;
+
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='f';
+		int b=a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+2);
+		
+		
+		int i=10;
+		double d=(double) i;
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(i+20.0);
+		
+		int j=10;
+		System.out.println(j/2.0);
+		System.out.println(j+2.0);
+		
+		int k=12;
+		float l=2.0f;
+		float y=k/l;
+		System.out.println(y);
+
+	}
+
+}
